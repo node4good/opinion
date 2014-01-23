4e0f103a3f0c347f9e7db2a76ed30fe671750b9d
Refael Ackermann <refael@empeeric.com>
2014-01-24 01:32:17 +0200

    more tree tweaking

--- example/kitchensink/app.js
+++ example/kitchensink/app.js
@@ -1,25 +1,25 @@
 "use strict";
 Error.stackTraceLimit = Infinity;
 process.chdir(__dirname);
-var opinions = require('../..');
+var opinion = require('../../');
 var conf = {
     PORT: process.env.PORT || 80
 };
-var app = opinions({
-    middlewareOrder: opinions.DEFAULT_MIDDLEWARE_ORDER,
-    keys: ['hell yeah'],
-    statics: {
-        root: 'assets'
-    },
+var app = opinion({
+    middlewareOrder: opinion.DEFAULT_MIDDLEWARE_STACK,
+    keys: ['78fd9fe83f2af46f2a8b567154db8d2a'],
+    statics: 'assets',
     render: ['views', 'dust']
 });
 
 
-app.use(function* () {
-    this.body = yield this.render('hello-world');
-});
+app.use(
+    function* () {
+        this.body = yield this.render('hello-world');
+    }
+);
 
 
-var server = app.listen(conf.PORT, function () {
-    console.log("Server listening on %s", server._connectionKey);
+app.listen(conf.PORT, function () {
+    console.log("Server listening on %s", this._connectionKey);
 });
