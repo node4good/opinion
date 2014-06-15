bcdf2da44a1029f1ddfdb6f3cf5195177688fc77
Refael Ackermann <refael@empeeric.com>
2014-06-15 20:46:48 +0300

    Finally decided on a routing mechanism + socket.io

--- example/kitchensink/app.js
+++ example/kitchensink/app.js
@@ -2,24 +2,29 @@
 Error.stackTraceLimit = Infinity;
 process.chdir(__dirname);
 var opinion = require('../../');
-var conf = {
-    PORT: process.env.PORT || 80
-};
+var conf = require('./conf');
+
 var app = opinion({
     middlewareOrder: opinion.DEFAULT_MIDDLEWARE_STACK,
     keys: ['78fd9fe83f2af46f2a8b567154db8d2a'],
     statics: 'assets',
-    render: ['views', 'dust']
+    render: ['views', 'dust'],
+    socketio: { serveClient:true }
 });
 
 
-app.use(
+
+app.get('/',
     function* () {
         this.body = yield this.render('hello-world');
     }
 );
 
-
 app.listen(conf.PORT, function () {
     console.log("Server listening on %s", this._connectionKey);
 });
+
+
+setInterval(function () {
+    app.socketio.emit('gaga', JSON.stringify(process.memoryUsage()))
+}, 3000);
