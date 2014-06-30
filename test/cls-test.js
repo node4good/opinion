ecd7dfaca2b36dbb41d7ace019e31fb8584d832f
Refael Ackermann <refael@empeeric.com>
2014-06-30 19:31:59 +0300

    cls test + app.mock()

--- test/cls-test.js
+++ test/cls-test.js
@@ -3,7 +3,17 @@ describe("CLS testing", function () {
     var opinion = require('../');
     it("CLS namespace should exist", function (done) {
         var app = opinion();
-        expect(global.opinionCLS);
-        done();
+        app.onerror = done;
+
+        app.use(function* () {
+            yield function* () {
+                expect(global.opinionCLS.get('webContext')).equal(this);
+            };
+            done();
+        });
+
+        expect(global).to.have.property('opinionCLS');
+
+        app.mock();
     });
 });
